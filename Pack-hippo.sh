feeec40da4ea3cb416de9f3323cacecd463a010a
IA,EMON <34213225+MRFOX777@users.noreply.github.com>
2023-01-20 23:21:21 +0600

    Add files via upload

---
+++ Pack-hippo.sh
@@ -0,0 +1,147 @@
+#!/data/data/com.termux/files/usr/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " Pack hippo * basic all package in one script "
+echo "---------------------------------------------------------------------------------"
+
+echo
+
+echo  "★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★"
+echo   "
+ ██▓███   ▄▄▄       ▄████▄   ██ ▄█▀    ██░ ██  ██▓ ██▓███   ██▓███   ▒█████  
+▓██░  ██▒▒████▄    ▒██▀ ▀█   ██▄█▒    ▓██░ ██▒▓██▒▓██░  ██▒▓██░  ██▒▒██▒  ██▒
+▓██░ ██▓▒▒██  ▀█▄  ▒▓█    ▄ ▓███▄░    ▒██▀▀██░▒██▒▓██░ ██▓▒▓██░ ██▓▒▒██░  ██▒
+▒██▄█▓▒ ▒░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄    ░▓█ ░██ ░██░▒██▄█▓▒ ▒▒██▄█▓▒ ▒▒██   ██░
+▒██▒ ░  ░ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄   ░▓█▒░██▓░██░▒██▒ ░  ░▒██▒ ░  ░░ ████▓▒░
+▒▓▒░ ░  ░ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒    ▒ ░░▒░▒░▓  ▒▓▒░ ░  ░▒▓▒░ ░  ░░ ▒░▒░▒░ 
+░▒ ░       ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░    ▒ ░▒░ ░ ▒ ░░▒ ░     ░▒ ░       ░ ▒ ▒░ 
+░░         ░   ▒   ░        ░ ░░ ░     ░  ░░ ░ ▒ ░░░       ░░       ░ ░ ░ ▒  
+               ░  ░░ ░      ░  ░       ░  ░  ░ ░                        ░ ░  
+                   ░                                                         
+"               
+ echo   "  "                                                                    
+echo   "#########################################################################"
+
+echo
+echo
+echo
+echo
+
+echo "."
+
+apt update && apt upgrade -y
+pkg update && pkg upgrade -y
+#update & upgrade in progress
+termux-setup-storage -y
+                          #installation..
+apt-get install cmatrix -y
+cmatrix
+apt install python -y                    
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+apt install python3 -y
+apt install nmap -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+apt install wcalc -y
+apt install php -y 
+apt install perl -y 
+apt install openssl -y
+apt install bmon -y
+apt install scipy -y
+apt install numpy -y
+apt install sl -y
+apt install openssh -y
+apt install w3m -y
+apt install zsh -y
+pkg install php -y
+apt install cmatrix -y
+apt install nodejs -y
+apt install bmon -y
+apt install jq -y
+pip install lolcat -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+pip install --upgrade pip
+pip3 install colorama
+pip install future -y
+pip install requests -y
+
+cd
+git clone https://github.com/cyberkallan/clipboardme
+
+cd Clipboardme
+
+chmod +x *
+
+cd
+
+
+
+git clone git://github.com/htr-tech/zphisher.git
+
+git clone https://github.com/Ha3MrX/DDos-Attack
+
+cd DDos-Attack
+
+chmod +x ddos-attack.py
+
+cd
+git clone https://github.com/HatBashBR/HatCloud
+cd
+
+git clone https://github.com/jaykali/maskphish
+
+cd
+
+
+
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd
+
+pkg install nmap
+
+cd
+
+git clone https://github.com/jaykali/maskphish
+
+cd
+
+
+
+
+
+
+
+git clone https://github.com/remo7777/T-Header.git
+
+cd T-Header/
+
+bash t-header.sh
+
+rm -rf T-Header
+
+cd ~/T-header && bash t-header.sh --remove && exit
+
+
+clear
+echo " If You Connected To Internet Then  "
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY  "
+echo "🇹​​​​​🇭​​​​​🇮​​​​​🇸​​​​​ 🇸​​​​​🇨​​​​​🇷​​​​​🇮​​​​​🇵​​​​​🇹​​​​​ 🇮​​​​​🇸​​​​​ 🇲​​​​​🇦​​​​​🇩​​​​​🇪​​​​​ 🇧​​​​​🇾​​​​​🇮​​​​​🇦​​​​​.🇪​​​​​🇲​​​​​🇴​​​​​🇳​​​​​"
+
+
+
+sl
+exit
